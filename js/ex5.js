253d264de202ad0f9bf96976c088e067376125fd
hatsue07 <boulier.lydiane@outlook.fr>
2025-12-18 17:38:57 +0100

    Add files via upload

--- js/ex5.js
+++ js/ex5.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { //on charge les éléments du DOM
 const question1_phare = document.getElementById("question1_phare");
 const question1_nez = document.getElementById("question1_nez");
 const question1_faon = document.getElementById("question1_faon");
@@ -15,41 +15,37 @@ const question3_scie = document.getElementById("question3_scie");
 const question3_mat = document.getElementById("question3_mat");
 
 const bouton_fini = document.getElementById("bouton_fini");
-
+//on récupère les éléments du DOM
 
 const lien = document.getElementById("page_suivante");
 lien.style.visibility="hidden";
+//Lien qui apparaitra lorsque l'utilisateur aura réussi l'ensemble des exercices
 
-let nb_bonnes_reponses= 0;
 
-//bonnes réponses
+//Bonne réponses
 question1_phare.addEventListener("change", () => {
 	if(question1_phare.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1; 
 		question1_phare.disabled = true; 
 	}
-});
-// si la réponse est bonne on incrémente
-// empèche de décocher la case
+});// On vérouille la case quand elle est cochée
+
 	
 
 question1_faon.addEventListener("change", () => {
 	if(question1_faon.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1;
-		question1_faon.disabled = true;
+		nb_bonnes_reponses = nb_bonnes_reponses + 1; //icrémentation du compteur nb_bonnes_reponses
+		question1_faon.disabled = true; // Vérouillage de la case
 	}
 });
 
 question2_des.addEventListener("change", () => {
 	if(question2_des.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1;
 		question2_des.disabled = true;
 	}
 });
 
 question2_pas.addEventListener("change", () => {
 	if(question2_pas.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1;
 		question2_pas.disabled = true;
 	}
 });
@@ -57,44 +53,39 @@ question2_pas.addEventListener("change", () => {
 		
 question2_nez.addEventListener("change", () => {
 	if(question2_nez.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1;
 		question2_nez.disabled = true;
 	}
 });
 
 question3_phare.addEventListener("change", () => {
 	if(question3_phare.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1;
-		question3_phare.disabled = true;
+		question3_phare.disabled = true; // Vérouillage de la case
 	}
 });
 
-
-
 question3_mat.addEventListener("change", () => {
 	if(question3_mat.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1;
 		question3_mat.disabled = true;
 	}
 });
 
 question3_scie.addEventListener("change", () => {
 	if(question3_scie.checked){
-		nb_bonnes_reponses = nb_bonnes_reponses + 1;
 		question3_scie.disabled = true;
 	}
 });
 
-//mauvaises réponses
+//Mauvaises réponses
 question1_nez.addEventListener("change", () => {
 	if(question1_nez.checked){
-		question1_nez.disabled = true;
+		question1_nez.disabled = true;//Vérouillage de la case
 	}
 });
 
+
 question1_hotte.addEventListener("change", () => {
 	if(question1_hotte.checked){
-		question1_hotte.disabled = true;
+		question1_hotte.disabled = true; 
 	}
 });
 
@@ -112,17 +103,32 @@ question3_faon.addEventListener("change", () => {
 
 
 bouton_fini.addEventListener("click", () => {
-	if (nb_bonnes_reponses == 8) {
-	window.alert("bravo ! tu peux passer à l'exercice suivant");
-    lien.style.visibility="visible";
-	}else{
-	alert("il y a une erreur, recommence !"); 
-	//si l'utilisateur a faux, il recommence a zéro
-	//on remet le compteur à zéro, et on réactive les cases
-	nb_bonnes_reponses = 0;
 	
-	question1_phare.checked =false;
-	question1_phare.disabled =false;
+	let erreurs = []; //initialisation d'un tableau d'erreurs
+	
+	if(!question1_phare.checked || !question1_faon.checked || question1_nez.checked || question1_hotte.checked){
+		erreurs.push("question 1") 
+		}//définition des cases qui doivent être cochées ou non, si il y a une erreur, on insére le nom de la question dans le tableau d'erreur
+		
+	if(!question2_des.checked || !question2_pas.checked || !question2_nez.checked || question2_mat.checked){
+		erreurs.push("question 2")
+		}
+	
+	if(!question3_phare.checked || !question3_scie.checked || !question3_mat.checked || question3_faon.checked ){
+		erreurs.push("question 3")
+		}
+	
+	if (erreurs.length === 0){ //si il n'y a pas d'erreur, un message confirme la réussite de l'exercice
+		window.alert("Bravo ! Tu peux maintenant retourner sur la page d'accueil et refaire les exercices si tu le souhaites!");
+		lien.style.visibility="visible";//le lien apparait
+		}
+	
+	else{
+	alert("il y a une erreur dans la question :" +erreurs.join(",") +" Recommence !"); //message si il y a une erreur avec l'endroit où elle a été faite
+	
+	
+	question1_phare.checked =false;// On décoche la case
+	question1_phare.disabled =false;// On déverrouille la case
 
 	question1_nez.checked = false;
 	question1_nez.disabled = false;
@@ -139,8 +145,8 @@ bouton_fini.addEventListener("click", () => {
 	question2_mat.checked = false;
 	question2_mat.disabled = false;
 
-	question2_pas.checked = false;
-	question2_pas.disabled = false;
+	question2_pas.checked = false;// On décoche la case
+	question2_pas.disabled = false;// On déverrouille la case
 
 	question2_nez.checked = false;
 	question2_nez.disabled = false;
@@ -154,8 +160,8 @@ bouton_fini.addEventListener("click", () => {
 	question3_scie.checked = false;
 	question3_scie.disabled = false;
 
-	question3_mat.checked = false;
-	question3_mat.disabled = false;
+	question3_mat.checked = false;// On décoche la case
+	question3_mat.disabled = false;// On déverrouille la case
 
 	}
 });
